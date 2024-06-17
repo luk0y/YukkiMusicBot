6512c38b196f7c384644adbe9b28c04e5983190a
luk0y <45989946+luk0y@users.noreply.github.com>
2024-06-17 21:02:29 +0530

    session_name -> name
    Updates session names and param

--- YukkiMusic/core/call.py
+++ YukkiMusic/core/call.py
@@ -61,7 +61,7 @@ class Call(PyTgCalls):
         self.userbot1 = Client(
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            session_name=str(config.STRING1),
+            name="SESSION_ONE",
         )
         self.one = PyTgCalls(
             self.userbot1,
@@ -70,7 +70,7 @@ class Call(PyTgCalls):
         self.userbot2 = Client(
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            session_name=str(config.STRING2),
+            name="SESSION_TWO",
         )
         self.two = PyTgCalls(
             self.userbot2,
@@ -79,7 +79,7 @@ class Call(PyTgCalls):
         self.userbot3 = Client(
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            session_name=str(config.STRING3),
+            name="SESSION_THREE",
         )
         self.three = PyTgCalls(
             self.userbot3,
@@ -88,7 +88,7 @@ class Call(PyTgCalls):
         self.userbot4 = Client(
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            session_name=str(config.STRING4),
+            name="SESSION_FOUR",
         )
         self.four = PyTgCalls(
             self.userbot4,
@@ -97,7 +97,7 @@ class Call(PyTgCalls):
         self.userbot5 = Client(
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            session_name=str(config.STRING5),
+            name="SESSION_FIVE",
         )
         self.five = PyTgCalls(
             self.userbot5,
